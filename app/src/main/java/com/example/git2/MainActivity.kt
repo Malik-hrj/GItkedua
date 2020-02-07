c0b8c47451be260f0b30598ad20ca5808cf9ecb7
Malik hrj <malikhrj2@gmail.com>
2020-02-07 10:27:09 +0700

    main yaaa

--- app/src/main/java/com/example/git2/MainActivity.kt
+++ app/src/main/java/com/example/git2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.git2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Ini activity punya malik yaa", Toast.LENGTH_LONG).show()
     }
 }
